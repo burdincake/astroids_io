03b5bc5199ef57f32ea211ef433ec8e85f2fcc71
samsungplay <samsungplayee@gmail.com>
2020-11-22 23:27:06 +0900

    E

---
+++ samsungplay/RigidBody2D.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var velocity = Vector2()
+export var accel_speed = 1000.0
+export var decel_speed = 0.45
+export var max_horizontal_velocity = 100.0
+export var max_vertical_velocity = 100.0
+var is_accel = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	is_accel = false
+	
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += delta * accel_speed
+		is_accel = true
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= delta * accel_speed
+		is_accel = true
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += delta * accel_speed
+		is_accel = true
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= delta * accel_speed
+		is_accel = true
+	if not is_accel:
+		velocity.x = lerp(velocity.x,0,decel_speed * delta)
+		velocity.y = lerp(velocity.y,0,decel_speed*delta)	
+	if velocity.x > 0:
+		velocity.x = min(velocity.x,max_horizontal_velocity)
+	else:
+		velocity.x = max(velocity.x,-max_horizontal_velocity)
+	if velocity.y > 0:
+		velocity.y = min(velocity.y,max_vertical_velocity)
+	else:
+		velocity.y = max(velocity.y,-max_vertical_velocity)
+	
+	
+	
+	move_and_slide(velocity)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
