87323b4766ea37bc2e7a34e0bdc625776294f0f0
burdincake <juneseokkwon@Juneui-MacBookPro.local>
2020-11-23 09:40:54 +0900

    add scene

--- burdincake/TextEdit.gd
+++ burdincake/TextEdit.gd
@@ -5,6 +5,7 @@ extends TextEdit
 # var a = 2
 # var b = "text"
 
+var scene = preload("res://samsungplay/Astroid_GUI.tscn").instance()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +17,7 @@ func _process(delta):
 	
 	if global.buttonPressed == true:
 		global.playerName = get_line(0)
+		get_tree().get_root().add_child(scene)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
